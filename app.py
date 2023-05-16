6dc2af25a19c2cc8d33f0843db7b5986ac5a7ac1
Zegnar0 <zegnarkow@gmail.com>
2023-05-16 12:10:55 +0200

    Création Sqlite + Visualisateur

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import sqlite3
 from flask import Flask , render_template
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    test = conn.execute('SELECT * FROM test').fetchall()
+    conn.close()
+    return render_template('index.html',test=test)
 
 
 @app.route('/game')
