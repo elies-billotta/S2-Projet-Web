da9be11af331f7a50b44488e103520b71b03f077
elies <elies.billtta@edu.univ-eiffel.fr>
2023-05-16 21:20:50 +0200

    j'essaie des choses

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import sqlite3
+from model import *
 from flask import Flask , render_template
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 #get a random movie from the database
 @app.route('/')
 def index():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    cur = connection.cursor()
-    test = cur.execute('SELECT * FROM film ORDER BY RANDOM() LIMIT 1')
-    connection.commit()
+    test = Film.get_random_film()
     return render_template('index.html',test=test)
 
 @app.route('/game')
--- model.py
+++ model.py
@@ -1,11 +1,21 @@
+import sqlite3
+
 class Film : 
-    def __init__(self, name, image, nom, description, difficulte):
-        self.name = name
-        self.image = image
+    def __init__(self, nom,  image, description, difficulte):
         self.nom = nom
+        self.image = image
         self.description = description
         self.difficulte = difficulte
 
+    def get_random_film():
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        cur = connection.cursor()
+        res = cur.execute('SELECT * FROM film ORDER BY RANDOM() LIMIT 1').fetchone()
+        movie = Film(res[1], res[2], res[3], res[4])
+        print(movie)
+        return movie
+    
 class Joueur : 
     def __init__(self, name, score):
         self.name = name
